e75656f25a9f0b84615f3dba1191247ede3dc5db
348903204 <348903204@gapps.yrdsb.ca>
2022-06-10 10:34:21 -0400

    Update project.greenfoot
    he do the animation wee woo thingy

--- Archer.java
+++ Archer.java
@@ -6,10 +6,31 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @version (a version number or a date)
  */
 public class Archer extends Actor
-{
+{ 
     int mouseDifference;
     GreenfootImage[] archers = new GreenfootImage[6];
 
+   
+    public Archer()
+    {
+        for(int i=0; i <archers.length; i++)
+        {
+            archers[i] = new GreenfootImage("images/archersfolder/archers" + i + ".png");
+        }
+        setImage(archers[0]);
+    }
+    
+    /**
+     * Shooting animation for the archer
+     */
+    int imageIndex = 1;
+    public void shoot()
+    {
+        setImage(archers[imageIndex]);
+        imageIndex++;
+        
+    }
+    
     /**
      * Act - do whatever the Archer wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -17,30 +38,28 @@ public class Archer extends Actor
     
     public void act()
     {
-        /**if (Greenfoot.mouseClicked(null))
-        *{
-        *    shoot();
-        *}
-        */
+        if (Greenfoot.mouseClicked(null))
+        {
+            for(int i = 0; i<5; i++)
+            {
+                shoot();
+                Greenfoot.delay(3);
+            }
+        }
+        
         MouseInfo mouse = Greenfoot.getMouseInfo();
         if (Greenfoot.mouseMoved(this.getWorld()))
         {
             if (getY() - mouse.getY() < 25)
             {
-                setImage("images/archersfolder/archers1.png");
+                setImage("images/archersfolder/archers0.png");
             }else
             {
-                setImage("images/archersfolder/archers12.png");
+                setImage("images/archersfolder/archers1.png");
             }
         }
         
     }
     
-    private void shoot()
-    {
-        for (int i = 0; i < 6; i++)
-        {
-            archers[i] = new GreenfootImage("images/archersfolder/archers" + i + ".png");
-        }
-    }
+    
 }
