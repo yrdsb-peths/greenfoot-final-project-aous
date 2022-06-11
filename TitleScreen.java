a2808998d585b98f44063e3fd2528ea9ceffe8d4
4ous <349738278@gapps.yrdsb.ca>
2022-06-11 14:45:22 -0400

    removed all the redundant code

--- TitleScreen.java
+++ TitleScreen.java
@@ -36,28 +36,7 @@ public class TitleScreen extends World
      */
     private void prepare()
     {
-        Archer archer = new Archer();
-        addObject(archer,135,232);
-        Arrow arrow = new Arrow();
-        addObject(arrow,338,157);
-        arrow.setLocation(302,153);
-        Target target = new Target();
-        addObject(target,449,218);
-        target.setLocation(466,229);
-        target.setLocation(455,233);
-        target.setLocation(466,229);
-        archer.setLocation(149,225);
-        arrow.setLocation(314,160);
         Label label = new Label("Press <SPACE> To Start", 40);
-        addObject(label,265,313);
-        label.setLocation(337,347);
-        label.setLocation(310,349);
-        label.setLocation(367,348);
-        label.setLocation(346,343);
-        label.setLocation(326,360);
-        label.setLocation(285,352);
-        label.setLocation(338,346);
-        label.setLocation(349,347);
-        label.setLocation(292,353);
+        addObject(label,292,353);
     }
 }
